53c6b8c579bf58be1f418739a1de10608a0ae4b1
Kubilay <kubilay.selin@gmail.com>
2022-12-12 17:16:44 -0800

    Data import and first run of a PARAFAC model

---
+++ StaRdom_pipeline.R
@@ -0,0 +1,119 @@
+library(staRdom) # folder
+library(data.table)
+cores <- detectCores(logical = FALSE)
+
+setwd("..")
+Samples <- eem_read("Horiba", recursive = TRUE, import_function = "aqualog")
+
+for (i in seq_along(Samples)) {
+  Samples[[i]][["x"]]<- Samples[[i]][["x"]][,41:1]
+}
+
+#eem_overview_plot(Samples, spp=9, contour = TRUE)
+Samples[[1]][["x"]]
+
+# THis takes the name of the folders in the Hitachi folder, then I pasted it in the path of the absorbance files
+BiblioDir = list.dirs(path = "Hitachi", full.names = TRUE, recursive = FALSE)
+abs=list.files(path=BiblioDir, pattern=".DX")
+
+write.csv(eem_metatemplate(Samples), file="metatable.csv", row.names=T)
+meta <- read.table("metatable.csv", header = TRUE, sep = ",", dec = ".", row.names=1)
+
+abs=sub(paste0(meta[,2],"/", meta[,1], ".dx"), pattern="Horiba", replacement="Hitachi")
+absorbance <- absorbance_read(abs, package="staRdom") # load csv or txt tables in folder
+names(absorbance)[-1] = c(substr(colnames(absorbance[-1]), 1, stop=nchar(colnames(absorbance[-1]))-3)) 
+
+#Auto write a metatable with file locations
+write.csv(eem_metatemplate(Samples, absorbance), file="metatable.csv", row.names=T)
+meta <- read.table("metatable.csv", header = TRUE, sep = ",", dec = ".", row.names=1)
+
+### Check for problematic samples
+problem <- eem_checkdata(eem_list=Samples, absorbance=absorbance, metadata=meta, error=T)
+
+#Absorbance baseline correction
+absorbance <- abs_blcor(absorbance,wlrange = c(680,700))
+
+eem_list <- eem_extend2largest(Samples, interpolation = 1, extend = FALSE)
+eem_raman_area(eem_list, blanks_only = T, average=F)
+
+eem_list <- eem_remove_blank(eem_list)
+eem_overview_plot(eem_list, spp=9, contour = TRUE)
+
+eem_list <- eem_ife_correction(eem_list,absorbance, cuvl = 2)
+
+eem_list<- eem_raman_normalisation2(eem_list, blank = "blank")
+eem_overview_plot(eem_list, spp=9, contour = TRUE)
+
+#remove balnks from the sample list
+eem_list <- eem_extract(eem_list, c("blank"),ignore_case = TRUE)
+absorbance <- dplyr::select(absorbance, -matches("nano|miliq|milliq|mq|blank", ignore.case = TRUE))
+
+#remove and interpolate scatter
+remove_scatter <- c(TRUE, TRUE, TRUE, TRUE)
+remove_scatter_width <- c(40,10,10,10)
+
+eem_list <- eem_rem_scat(eem_list, remove_scatter = remove_scatter, remove_scatter_width = remove_scatter_width)
+eem_overview_plot(eem_list[], spp=9, contour = TRUE)
+
+eem_list <- eem_interp(eem_list, cores = cores, type = 1, extend = FALSE)
+
+eem4peaks <- eem_smooth(eem_list, n = 4, cores = cores)
+eem_overview_plot(eem_list, spp=9, contour = TRUE)
+
+bix <- eem_biological_index(eem4peaks)
+coble_peaks <- eem_coble_peaks(eem4peaks)
+fi <- eem_fluorescence_index(eem4peaks)
+hix <- eem_humification_index(eem4peaks, scale = TRUE)
+
+
+library(tidyverse)
+indices_peaks <- bix %>%
+  full_join(coble_peaks, by = "sample") %>%
+  full_join(fi, by = "sample") %>%
+  full_join(hix, by = "sample")
+
+setDT(indices_peaks)[, c("sample_date", "replicate", "col_no") := tstrsplit(sample, "_")]
+
+slope_parms <- abs_parms(absorbance, cuvl = 1, cores = cores)
+abs_params=merge(indices_peaks, slope_parms, by="sample")
+
+list(abs_params, abs_params$sample_date)
+
+for (){
+  
+}
+
+ggplot(abs_params)+
+  facet_grid(~sample_date)+
+  geom_line(aes(x=col_no, y=bix, group=replicate))
+
+#### PARAFAC analysis ####
+eem_list=eem_red2smallest (eem_list)
+#eem_list %>% 
+# eem_extract(sample = "", keep = TRUE) %>%
+#ggeem(contour = TRUE)
+
+dim_min <- 3
+dim_max <- 7
+
+nstart <- 25 # number of similar models from which best is chosen
+maxit = 5000 # maximum number of iterations in PARAFAC analysis
+ctol <- 10^-6 # tolerance in PARAFAC analysis
+
+# calculating PARAFAC models, one for each number of components
+pf1 <- eem_parafac(eem_list, comps = seq(dim_min,dim_max), normalise = FALSE, const = c("uncons", "uncons", "uncons"), maxit = maxit, nstart = nstart, ctol = ctol, cores = cores)
+pf1 <- lapply(pf1, eempf_rescaleBC, newscale = "Fmax")
+eempf_compare(pf1, contour = TRUE)
+
+
+# same model but using non-negative constraints
+pf1n <- eem_parafac(eem_list, comps = seq(dim_min,dim_max), normalise = FALSE, const = c("nonneg", "nonneg", "nonneg"), maxit = maxit, nstart = nstart, ctol = ctol, cores = cores)
+pf1n <- lapply(pf1n, eempf_rescaleBC, newscale = "Fmax")
+eempf_compare(pf1n, contour = TRUE)
+
+
+
+####
+
+
+
