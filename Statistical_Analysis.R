e1d6bde3d89c3a21d9be3a3552e5cb337a977a7d
Selin Kubilay <kubilay@igb-berlin.de>
2022-12-19 15:47:47 -0800

    - data cleaning corrections - panel plot of the pca results

--- Statistical_Analysis.R
+++ Statistical_Analysis.R
@@ -7,11 +7,10 @@ data$replicate=factor(data$replicate, levels = c("reservoir", "Reservoir","A", "
 data$col_no=factor(data$col_no, levels = c("Reservoir", "C1", "C2", "C3"))
 
 #Merge the tables for the PCA master table
-pca_data=data[sample== unique(data[samples, on = .(sample)][,sample])]
-pca_data=unique(pca_data[,-c("replicate")])#Extract the replicated reservoir values for the pca
+pca_data=unique(data, by="sample")#Exclude the replicated reservoir values for the pca
 
 pca_data[is.na(pca_data)]<-0
-wine.pca <- prcomp(pca_data[,-c("sample", "sample_date","col_no", "E4_E6")], scale. = TRUE) 
+wine.pca <- prcomp(pca_data[,-c("sample", "replicate","sample_date","col_no", "E4_E6")], scale. = TRUE) 
 summary(wine.pca)
 
 # PCA plots
@@ -39,6 +38,18 @@ ggplot(pca_results, aes(x=pca_results$PC1_median, y=pca_results$PC2_median))+
   scale_color_manual(values=ggthemes::tableau_div_gradient_pal()(seq(0, 1, length = 18)))+
   geom_segment(data = PCAloadings, aes(x = 0, y = 0, xend = (PC1*10), yend = (PC2*10)), arrow = arrow(length = unit(1/2, "picas")),color = "black") +
   annotate("text", x = (PCAloadings$PC1*10.4), y = (PCAloadings$PC2*10.4),
+           label = PCAloadings$Variables)+
+  theme_classic()+
+  guides(fill="legend")
+
+# Plotting according to the sampling date with before reversal after reversal facets
+ggplot(pca_data, aes(x=wine.pca$x[,1], y=wine.pca$x[,2]))+
+  facet_grid(~sample_date>"S10", scales="free_x", labeller=as_labeller(c('FALSE'="Before Reversal", 'TRUE'="After Reversal")))+
+  geom_point(aes(color=pca_data$sample_date, shape=col_no),  size=4)+
+  scale_color_manual(values=ggthemes::tableau_div_gradient_pal()(seq(0, 1, length = 18)))+
+  geom_segment(data = PCAloadings, aes(x = 0, y = 0, xend = (PC1*10), yend = (PC2*10)), 
+               inherit.aes=FALSE, arrow = arrow(length = unit(1/2, "picas")),color = "black") +
+  annotate("text", aes(x = (PCAloadings$PC1*10.4), y = (PCAloadings$PC2*10.4), inherit.aes=FALSE),
            label = PCAloadings$Variables)+
   theme_classic()+
   guides(fill="legend")
