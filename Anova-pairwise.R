da089fd84914c1a65be6a2a1a1010b5f1622fa58
Selin Kubilay <kubilay@igb-berlin.de>
2023-01-10 21:31:41 -0800

    Anova on the selected samples

--- Anova-pairwise.R
+++ Anova-pairwise.R
@@ -7,7 +7,7 @@ data3[,paste0(cols):=lapply(.SD, FUN=as.character), .SDcols=cols]
 data3=subset(data3, subset=!(sample_date>"S10" & replicate=="O"))
 
 cell_counts= read.csv("C:/Users/c7701233/Nextcloud/Column-Experiment/EEA/Cell_Counts_standardized.csv")
-pca_data3=unique(data3[sample_date%in% c("S08", "S10", "S11", "S12", "S13", "S14", "S17", "S19")], by="sample")
+pca_data3=unique(data3[sample_date%in% c("S08", "S10", "S11", "S12", "S13", "S14", "S17",  "S16", "S19")], by="sample")
 
 reps=as.character(unique(pca_data3[sample_date=="S11"]$replicate)) #takes the replicates of that sampling date
 anova_first=pca_data3[sample_date%in% c("S08", "S11") & replicate %in% reps] # copies only the matching replicates to the data frame
@@ -20,3 +20,11 @@ TukeyHSD(aov)
 anova_first=anova_first[replicate!="Reservoir"]
 pairwise.t.test(x=anova_first$fi, g=interaction(anova_first$sample_date, anova_first$col_no), 
                 p.adjust.method = "bonferroni", paired = F, pool.sd = T)
+
+# Anova uing only matching replicates
+anova_second=pca_data3[sample_date%in% c("S10", "S13", "S16", "S19") & replicate!="Reservoir"] # copies only the matching replicates to the data frame
+anova_second=anova_second[!(sample_date=="S13" & replicate%in%c("A","B", "C", "F", "G", "I", "P"))]
+
+aov2=aov(E2_E3~sample_date*col_no, data=anova_second)
+summary(aov2)
+TukeyHSD(aov2)
